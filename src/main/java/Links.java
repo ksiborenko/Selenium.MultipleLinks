3d444fab1e05e584d3689df6257e097e4621dc31
konrad <ksiborenko@gmail.com>
2021-04-23 11:06:04 +0200

    Multiple Links with: .findElements(By.tagName)
    Opening links in separate tabs with: Keys.chord(Keys.CONTROL, Keys.ENTER);
    
    Switching to different tabs with: Set<String> urls = this.driver.getWindowHandle()

--- src/main/java/Links.java
+++ src/main/java/Links.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Links {
 
@@ -18,6 +21,17 @@ public class Links {
         System.out.println(links.size());
         WebElement footer = this.driver.findElement(By.xpath("//table[@class='gf-t']/tbody/tr/td[1]/ul"));
         List<WebElement> footerList = footer.findElements(By.tagName("a"));
+        String keys = Keys.chord(Keys.CONTROL, Keys.ENTER);
+        Set<String> urls = null;
+        Iterator<String> iterate = null;
+        for (int x = 0; x < footerList.size(); x++) {
+            footerList.get(x).sendKeys(keys);
+            urls.add(this.driver.getWindowHandle());
+            iterate = urls.iterator();
+            if (iterate.hasNext()) {
+                iterate.next();
+            }
+        }
     }
 }
 
